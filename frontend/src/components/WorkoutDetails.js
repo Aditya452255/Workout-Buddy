594f2526f9c3183286efede90d55f0c98ed1fbf9
Aditya45225 <adityajain0322@gmail.com>
2025-12-09 20:31:46 +0530

    Configure for Vercel and Render deployment with all frontend files

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useWorkoutContext } from '../hooks/useWorkoutContext'
+import { useAuthContext } from '../hooks/useAuthContext';
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutContext();
+  const { user } = useAuthContext();
+  const handleClick = async () => {
+
+    if (!user) {
+      return;
+    }
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts/${workout._id}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    });
+    const json = await response.json();
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json});
+    }
+  }
+  return (
+    <div className='workout-details'>
+        <h4>{workout.title}</h4>
+        <p><strong>Load:(in kgs): </strong>{workout.load}</p>
+        <p><strong>Reps: </strong>{workout.reps}</p>
+        <p><strong>Created At: </strong>{new Date(workout.createdAt).toLocaleString()}</p>
+        <span  onClick={handleClick}>delete</span>
+        
+    </div>
+  )
+}
+
+export default WorkoutDetails
